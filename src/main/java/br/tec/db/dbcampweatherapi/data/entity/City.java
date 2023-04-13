b52281a4dcdf0d30479de11769a791a858c8e156
Rafael da Silva Santos <05202198155@faqi.edu.br>
2023-04-13 10:44:19 -0300

    :sparkles: feat: Criação das entidades Java

---
+++ src/main/java/br/tec/db/dbcampweatherapi/data/entity/City.java
@@ -0,0 +1,37 @@
+package br.tec.db.dbcampweatherapi.data.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"cityId", "state"})
+@Data
+@Entity
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore // TODO excluir anotação e retirar o campo no DTO (record)
+    @Column(name = "city_id")
+    private Long cityId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @JsonIgnore // TODO excluir anotação e retirar o campo no DTO (record) ;
+    @Column(name = "state")
+    private String state;
+
+    @OneToMany(mappedBy = "city",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    List<CityWeatherDateList> weatherDateLists = new ArrayList<>();
+
+}
