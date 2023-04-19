a23a538696e1daf7c3ae67acf8c338bf56f3340a
Rafael da Silva Santos <05202198155@faqi.edu.br>
2023-04-19 14:44:43 -0300

    :recycle: refactor: Tabela de previsões recriada
    Retornada as previsões de daytime e nightime enum como varchar, pois anotações `@Enumerated(EnumType.STRING)` nas entidades já fazem o trabalho dos enum que haviam sido criado manualmente com SQL.

---
+++ src/main/resources/db/migration/V11__CREATE_INSERT_DROP_RENAME_TABLE_WEATHER.sql
@@ -0,0 +1,23 @@
+CREATE TABLE city_weather_date_list_temp (
+                                             weather_id UUID NOT NULL PRIMARY KEY,
+                                             city_id BIGINT NOT NULL,
+                                             date DATE NOT NULL,
+                                             daytime_enum VARCHAR(40) NOT NULL,
+                                             nighttime_enum VARCHAR(40) NOT NULL,
+                                             max_degrees INTEGER NOT NULL,
+                                             min_degrees INTEGER NOT NULL,
+                                             precipitation INTEGER NOT NULL,
+                                             humidity INTEGER NOT NULL,
+                                             wind_speed INTEGER NOT NULL,
+                                             FOREIGN KEY (city_id) REFERENCES city(city_id)
+);
+
+-- Copying the data from the original table to the temporary table
+INSERT INTO city_weather_date_list_temp
+SELECT * FROM city_weather_date_list;
+
+-- Dropping the original table
+DROP TABLE city_weather_date_list;
+
+-- Renaming the temporary table with the original table name
+ALTER TABLE city_weather_date_list_temp RENAME TO city_weather_date_list;
