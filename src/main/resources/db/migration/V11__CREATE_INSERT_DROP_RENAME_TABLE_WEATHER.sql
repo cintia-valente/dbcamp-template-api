bfe0a47262addbdf1ee50f7ad55dd610559ee265
Rafael da Silva Santos <05202198155@faqi.edu.br>
2023-04-19 15:27:30 -0300

    :ok_hand: style: Arrumando indentação do SQL.

--- src/main/resources/db/migration/V11__CREATE_INSERT_DROP_RENAME_TABLE_WEATHER.sql
+++ src/main/resources/db/migration/V11__CREATE_INSERT_DROP_RENAME_TABLE_WEATHER.sql
@@ -1,15 +1,15 @@
 CREATE TABLE city_weather_date_list_temp (
-                                             weather_id UUID NOT NULL PRIMARY KEY,
-                                             city_id BIGINT NOT NULL,
-                                             date DATE NOT NULL,
-                                             daytime_enum VARCHAR(40) NOT NULL,
-                                             nighttime_enum VARCHAR(40) NOT NULL,
-                                             max_degrees INTEGER NOT NULL,
-                                             min_degrees INTEGER NOT NULL,
-                                             precipitation INTEGER NOT NULL,
-                                             humidity INTEGER NOT NULL,
-                                             wind_speed INTEGER NOT NULL,
-                                             FOREIGN KEY (city_id) REFERENCES city(city_id)
+     weather_id UUID NOT NULL PRIMARY KEY,
+     city_id BIGINT NOT NULL,
+     date DATE NOT NULL,
+     daytime_enum VARCHAR(40) NOT NULL,
+     nighttime_enum VARCHAR(40) NOT NULL,
+     max_degrees INTEGER NOT NULL,
+     min_degrees INTEGER NOT NULL,
+     precipitation INTEGER NOT NULL,
+     humidity INTEGER NOT NULL,
+     wind_speed INTEGER NOT NULL,
+     FOREIGN KEY (city_id) REFERENCES city(city_id)
 );
 
 -- Copying the data from the original table to the temporary table
